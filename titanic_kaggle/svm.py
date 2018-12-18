9b1b5ffc8a851dc79b7b18451484228129e60d29
vogdb <vozhdb@gmail.com>
2018-12-18 21:40:19 +0300

    refactor svm clf

--- titanic_kaggle/svm.py
+++ titanic_kaggle/svm.py
@@ -1,11 +1,11 @@
-from scipy.stats import expon, reciprocal
+from scipy.stats import expon, reciprocal, uniform
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVC
-
+import numpy as np
 from titanic_kaggle.data_processing import MostFrequentImputer, AgeImputer, NewColumnsTransformer, load_data
 from titanic_kaggle.estimator_serialize import EstimatorSerialize
 
@@ -23,7 +23,7 @@ def create_prepare_pipeline():
             ('scale', StandardScaler())
         ]), ['Fare']),
         ('age', StandardScaler(), ['Age']),
-        ('cat', OneHotEncoder(sparse=False), ['Pclass', 'Sex', 'AgeBucket']),
+        ('cat', OneHotEncoder(sparse=False), ['Pclass', 'Sex']),
         ('embarked', embarked_transformer, ['Embarked']),
     ], remainder='drop')
 
@@ -36,21 +36,33 @@ def create_prepare_pipeline():
 
 def main():
     X_train, y_train, X_test = load_data()
-    prepare_pipeline = create_prepare_pipeline()
-    X_train = prepare_pipeline.fit_transform(X_train)
 
-    svm_clf = SVC()
-    param_distribs = {
-        'kernel': ['linear', 'rbf'],
-        'C': reciprocal(20, 20000),
-        'gamma': expon(scale=1.0),
+    # param_distribs = {
+    #     'kernel': ['rbf', 'poly', 'sigmoid'],
+    #     'C': reciprocal(.1, 100),
+    #     'gamma': reciprocal(.001, 1),
+    # }
+    # search = RandomizedSearchCV(
+    #     SVC(), param_distributions=param_distribs,
+    #     n_iter=50, cv=5, scoring='accuracy', n_jobs=-1
+    # )
+    param_grid = {
+        'kernel': ['rbf'],
+        'gamma': np.arange(0.01, 0.11, 0.01),
+        'C': np.arange(1, 11),
     }
-    search = RandomizedSearchCV(
-        svm_clf, param_distributions=param_distribs,
-        n_iter=20, cv=5, scoring='accuracy', n_jobs=-1
+    search = GridSearchCV(
+        SVC(), param_grid=param_grid,
+        cv=5, scoring='accuracy', n_jobs=-1
     )
-    search.fit(X_train, y_train)
+    pipeline = Pipeline([
+        ('prepare', create_prepare_pipeline()),
+        ('search', search),
+    ])
+    pipeline.fit(X_train, y_train)
     EstimatorSerialize.save_estimator('svm', search.best_estimator_)
+    print(search.best_estimator_)
+    print(search.best_score_)
 
 
 if __name__ == '__main__':
