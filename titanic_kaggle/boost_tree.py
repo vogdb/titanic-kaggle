c2f944f02cafbf523a8441efd0778576e1230c80
vogdb <vozhdb@gmail.com>
2018-12-19 10:37:58 +0300

    same pipeline logic to mlp and boost_tree

--- titanic_kaggle/boost_tree.py
+++ titanic_kaggle/boost_tree.py
@@ -1,6 +1,7 @@
 import xgboost as xgb
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 
 from titanic_kaggle.data_processing import load_data
 from titanic_kaggle.estimator_serialize import EstimatorSerialize
@@ -9,8 +10,6 @@ from titanic_kaggle.rnd_forest import create_prepare_pipeline
 
 def main():
     X_train, y_train, X_test = load_data()
-    prepare_pipeline = create_prepare_pipeline()
-    X_train = prepare_pipeline.fit_transform(X_train)
     use_xgb = True
 
     if use_xgb:
@@ -29,15 +28,19 @@ def main():
         }
 
     search = GridSearchCV(
-        clf, param_grid=param_grid,
-        cv=5, scoring='accuracy', n_jobs=-1
+        clf, param_grid=param_grid, refit=True,
+        cv=5, scoring='accuracy', n_jobs=-1, error_score='raise'
     )
-    search.fit(X_train, y_train)
-    EstimatorSerialize.save_estimator('boost_tree', search.best_estimator_)
+    pipeline = Pipeline([
+        ('prepare', create_prepare_pipeline()),
+        ('search', search),
+    ])
+
+    pipeline.fit(X_train, y_train)
+    EstimatorSerialize.save_estimator('boost_tree', pipeline)
+
     print(search.best_estimator_)
     print(search.best_score_)
-    print(search.cv_results_['params'])
-    print(search.cv_results_['mean_test_score'])
 
 
 if __name__ == '__main__':
--- titanic_kaggle/mlp.py
+++ titanic_kaggle/mlp.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
 
 from titanic_kaggle.data_processing import load_data
 from titanic_kaggle.estimator_serialize import EstimatorSerialize
@@ -8,7 +9,8 @@ from titanic_kaggle.svm import create_prepare_pipeline
 
 
 def rand_array():
-    return np.random.randint(100, size=(np.random.randint(1, 4)))
+    # return np.random.randint(100, size=(np.random.randint(1, 4)))
+    return np.random.randint(50)
 
 
 def rand_array_list(n):
@@ -17,25 +19,28 @@ def rand_array_list(n):
 
 def main():
     X_train, y_train, X_test = load_data()
-    prepare_pipeline = create_prepare_pipeline()
-    X_train = prepare_pipeline.fit_transform(X_train)
+
     mlp_clf = MLPClassifier(
         [], solver='adam', batch_size=100, learning_rate='adaptive', alpha=.001, max_iter=1000,
         early_stopping=True, tol=.001, learning_rate_init=0.001, n_iter_no_change=100
     )
-
     param_grid = {
         'hidden_layer_sizes': rand_array_list(30),
     }
     search = GridSearchCV(
-        mlp_clf, param_grid=param_grid,
-        cv=5, scoring='accuracy', n_jobs=-1
+        mlp_clf, param_grid=param_grid, refit=True,
+        cv=5, scoring='accuracy', n_jobs=-1, error_score='raise'
     )
-    search.fit(X_train, y_train)
-    EstimatorSerialize.save_estimator('mlp', search.best_estimator_)
+    pipeline = Pipeline([
+        ('prepare', create_prepare_pipeline()),
+        ('search', search),
+    ])
+
+    pipeline.fit(X_train, y_train)
+    EstimatorSerialize.save_estimator('mlp', pipeline)
+
     print(search.best_estimator_)
     print(search.best_score_)
-    print(search.cv_results_['mean_test_score'])
 
 
 if __name__ == '__main__':
